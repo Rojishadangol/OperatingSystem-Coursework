d017ba599bad8c9c7c88a393b0a25cda8f8f0bc2
rojishadangol <rojishadangol@gmail.com>
2026-07-22 06:32:23 +0000

    update

--- Task1_Process_Management/task1.c
+++ Task1_Process_Management/task1.c
@@ -1,88 +1,107 @@
+/*
+    Task 1
+    Banking Management System
+    Process Management and Threading
+*/
+
 #include <stdio.h>
 #include <pthread.h>
 #include <unistd.h>
 
 int balance = 1000;
 pthread_mutex_t lock;
-pthread_mutex_t lock1, lock2;
 
-/* Thread Synchronization */
-
-void *deposit(void *arg)
+void *depositService(void *arg)
 {
+    printf("\nDeposit Service Started.\n");
+
     for(int i=0;i<5;i++)
     {
         pthread_mutex_lock(&lock);
+
         balance += 100;
-        printf("Deposit : %d\n", balance);
+
+        printf("Deposited Rs.100 | Balance = %d\n",balance);
+
         pthread_mutex_unlock(&lock);
+
+        usleep(100000);
     }
+
+    printf("Deposit Service Finished.\n");
+
     return NULL;
 }
 
-void *withdraw(void *arg)
+void *withdrawService(void *arg)
 {
+    printf("\nWithdrawal Service Started.\n");
+
     for(int i=0;i<5;i++)
     {
         pthread_mutex_lock(&lock);
+
         balance -= 50;
-        printf("Withdraw: %d\n", balance);
+
+        printf("Withdrawn Rs.50 | Balance = %d\n",balance);
+
         pthread_mutex_unlock(&lock);
+
+        usleep(100000);
     }
+
+    printf("Withdrawal Service Finished.\n");
+
     return NULL;
 }
 
-void *showBalance(void *arg)
+void *balanceService(void *arg)
 {
+    printf("\nBalance Inquiry Service Started.\n");
+
     for(int i=0;i<5;i++)
     {
         pthread_mutex_lock(&lock);
-        printf("Balance : %d\n", balance);
+
+        printf("Current Balance = %d\n",balance);
+
         pthread_mutex_unlock(&lock);
+
+        usleep(100000);
     }
+
+    printf("Balance Inquiry Finished.\n");
+
     return NULL;
 }
 
-void threadDemo()
-{
-    pthread_t t1,t2,t3;
-
-    pthread_mutex_init(&lock,NULL);
-
-    balance = 1000;
-
-    printf("\nInitial Balance = %d\n\n",balance);
-
-    pthread_create(&t1,NULL,deposit,NULL);
-    pthread_create(&t2,NULL,withdraw,NULL);
-    pthread_create(&t3,NULL,showBalance,NULL);
-
-    pthread_join(t1,NULL);
-    pthread_join(t2,NULL);
-    pthread_join(t3,NULL);
-
-    printf("\nFinal Balance = %d\n",balance);
-
-    pthread_mutex_destroy(&lock);
-}
-
-/* Round Robin  */
+/*-----------------------------
+    Round Robin Scheduling
+------------------------------*/
 
 void roundRobin()
 {
-    int bt[3]={5,8,6};
-    int rt[3],ct[3],tat[3],wt[3];
+    int bt[3] = {5,8,6};
+    int rt[3];
+    int ct[3], tat[3], wt[3];
 
-    int tq=2,time=0,done=0;
+    int tq = 2;
+    int time = 0;
+    int completed = 0;
 
-    float avgWT=0,avgTAT=0;
+    char process[3][25] =
+    {
+        "Deposit Service",
+        "Withdrawal Service",
+        "Balance Inquiry"
+    };
 
     for(int i=0;i<3;i++)
         rt[i]=bt[i];
 
-    printf("\nRound Robin Scheduling\n\n");
+    printf("\n Round Robin Scheduling \n");
 
-    while(done<3)
+    while(completed<3)
     {
         for(int i=0;i<3;i++)
         {
@@ -90,74 +109,83 @@ void roundRobin()
             {
                 if(rt[i]>tq)
                 {
-                    printf("P%d : %d units\n",i+1,tq);
+                    printf("%s executes from %d to %d\n",
+                    process[i],time,time+tq);
+
                     time+=tq;
                     rt[i]-=tq;
                 }
                 else
                 {
-                    printf("P%d : %d units (Finished)\n",i+1,rt[i]);
+                    printf("%s finished.\n",process[i]);
+
                     time+=rt[i];
                     rt[i]=0;
+
                     ct[i]=time;
-                    done++;
+                    completed++;
                 }
             }
         }
     }
 
-    printf("\nProcess\tBT\tCT\tTAT\tWT\n");
+    printf("\n-----------------------------------------\n");
+    printf("Service\t\tCT\tTAT\tWT\n");
+    printf("-----------------------------------------\n");
 
     for(int i=0;i<3;i++)
     {
         tat[i]=ct[i];
         wt[i]=tat[i]-bt[i];
 
-        avgWT+=wt[i];
-        avgTAT+=tat[i];
-
-        printf("P%d\t%d\t%d\t%d\t%d\n",
-               i+1,bt[i],ct[i],tat[i],wt[i]);
+        printf("%d\t\t%d\t%d\t%d\n",
+        i+1,ct[i],tat[i],wt[i]);
     }
-
-    printf("Average WT : %.2f\n",avgWT/3);
-    printf("Average TAT: %.2f\n",avgTAT/3);
 }
 
-/*  Deadlock Prevention */
+/*-----------------------------
+    Deadlock Prevention
+------------------------------*/
 
-void *thread1(void *arg)
+pthread_mutex_t accountLock;
+pthread_mutex_t databaseLock;
+
+void *transactionModule(void *arg)
 {
-    pthread_mutex_lock(&lock1);
-    printf("Thread 1 acquired Lock 1\n");
+    printf("\nTransaction Module waiting for Account Lock...\n");
+
+    pthread_mutex_lock(&accountLock);
 
     sleep(1);
 
-    pthread_mutex_lock(&lock2);
-    printf("Thread 1 acquired Lock 2\n");
+    pthread_mutex_lock(&databaseLock);
 
-    printf("Thread 1 Working...\n");
+    printf("Transaction Module Processing...\n");
 
-    pthread_mutex_unlock(&lock2);
-    pthread_mutex_unlock(&lock1);
+    pthread_mutex_unlock(&databaseLock);
+    pthread_mutex_unlock(&accountLock);
+
+    printf("Transaction Module Finished.\n");
 
     return NULL;
 }
 
-void *thread2(void *arg)
+void *databaseModule(void *arg)
 {
-    pthread_mutex_lock(&lock1);
-    printf("Thread 2 acquired Lock 1\n");
+    printf("\nDatabase Module waiting for Account Lock...\n");
+
+    pthread_mutex_lock(&accountLock);
 
     sleep(1);
 
-    pthread_mutex_lock(&lock2);
-    printf("Thread 2 acquired Lock 2\n");
+    pthread_mutex_lock(&databaseLock);
 
-    printf("Thread 2 Working...\n");
+    printf("Database Module Updating Records...\n");
 
-    pthread_mutex_unlock(&lock2);
-    pthread_mutex_unlock(&lock1);
+    pthread_mutex_unlock(&databaseLock);
+    pthread_mutex_unlock(&accountLock);
+
+    printf("Database Module Finished.\n");
 
     return NULL;
 }
@@ -166,22 +194,20 @@ void deadlockDemo()
 {
     pthread_t t1,t2;
 
-    pthread_mutex_init(&lock1,NULL);
-    pthread_mutex_init(&lock2,NULL);
+    pthread_mutex_init(&accountLock,NULL);
+    pthread_mutex_init(&databaseLock,NULL);
 
-    pthread_create(&t1,NULL,thread1,NULL);
-    pthread_create(&t2,NULL,thread2,NULL);
+    pthread_create(&t1,NULL,transactionModule,NULL);
+    pthread_create(&t2,NULL,databaseModule,NULL);
 
     pthread_join(t1,NULL);
     pthread_join(t2,NULL);
 
-    pthread_mutex_destroy(&lock1);
-    pthread_mutex_destroy(&lock2);
+    pthread_mutex_destroy(&accountLock);
+    pthread_mutex_destroy(&databaseLock);
 
-    printf("\nDeadlock avoided by locking resources in the same order.\n");
+    printf("\nDeadlock avoided using same lock order.\n");
 }
-
-/*  Main  */
 
 int main()
 {
@@ -189,20 +215,44 @@ int main()
 
     do
     {
-        printf("Task 1 - Process Management\n");
-        printf("1. Thread Synchronization\n");
+        
+        printf("   Banking Management System\n");
+        
+        printf("1. Banking Services (Threads)\n");
         printf("2. Round Robin Scheduling\n");
         printf("3. Deadlock Prevention\n");
         printf("4. Exit\n");
 
-        printf("Enter Choice: ");
+        printf("Enter Choice : ");
         scanf("%d",&choice);
 
         switch(choice)
         {
             case 1:
-                threadDemo();
+            {
+                pthread_t t1,t2,t3;
+
+                pthread_mutex_init(&lock,NULL);
+
+                printf("\nStarting Banking Services...\n");
+
+                pthread_create(&t1,NULL,depositService,NULL);
+                pthread_create(&t2,NULL,withdrawService,NULL);
+                pthread_create(&t3,NULL,balanceService,NULL);
+
+                pthread_join(t1,NULL);
+                pthread_join(t2,NULL);
+                pthread_join(t3,NULL);
+
+                pthread_mutex_destroy(&lock);
+
+                printf("\nFinal Account Balance : %d\n",balance);
+
+                printf("Synchronization using mutex completed.\n");
+                printf("All banking services completed.\n");
+
                 break;
+            }
 
             case 2:
                 roundRobin();
@@ -213,7 +263,7 @@ int main()
                 break;
 
             case 4:
-                printf("Program Ended.\n");
+                printf("\nProgram Ended.\n");
                 break;
 
             default:
@@ -224,3 +274,4 @@ int main()
 
     return 0;
 }
+
