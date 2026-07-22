3412ebbc4dd4a44172d77f69c1aa4f1716941133
rojishadangol <rojishadangol@gmail.com>
2026-07-22 07:36:41 +0000

    Multi-clients added

--- Task4_Network_programming/client.c
+++ Task4_Network_programming/client.c
@@ -1,81 +1,61 @@
+/*
+    Task 4
+    ABC Bank Client
+*/
+
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
 
-#define PORT 8080
-
 int main()
 {
     int clientSocket;
-    struct sockaddr_in serverAddr;
+
+    struct sockaddr_in server;
 
     char username[20];
     char password[20];
-    char message[100];
+    char transaction[100];
+    char reply[100];
 
     clientSocket = socket(AF_INET, SOCK_STREAM, 0);
 
-    if(clientSocket < 0)
-    {
-        printf("Socket creation failed.\n");
-        return 1;
-    }
+    server.sin_family = AF_INET;
+    server.sin_port = htons(5000);
+    server.sin_addr.s_addr = inet_addr("127.0.0.1");
 
-    serverAddr.sin_family = AF_INET;
-    serverAddr.sin_port = htons(PORT);
-    serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    connect(clientSocket, (struct sockaddr *)&server, sizeof(server));
 
-    if(connect(clientSocket,
-              (struct sockaddr*)&serverAddr,
-              sizeof(serverAddr)) < 0)
-    {
-        printf("Connection failed.\n");
-        return 1;
-    }
+    
+    printf("         ABC Bank Client\n");
+   
 
-    printf("Connected to Server Successfully.\n");
-
-    printf("Client Appllication..\n");
-    printf("Username: ");
+    printf("Username : ");
     scanf("%s", username);
 
-    printf("Password: ");
+    printf("Password : ");
     scanf("%s", password);
 
-    getchar();
-
-    if(strcmp(username,"admin") != 0 ||
-       strcmp(password,"1234") != 0)
+    if(strcmp(password,"12345")!=0)
     {
-        printf("Authentication Failed.\n");
-
+        printf("Invalid Login.\n");
         close(clientSocket);
         return 0;
     }
 
-    printf("Authentication Successful.\n");
+    printf("\nLogin Successful.\n");
 
-    printf("Enter Message: ");
-    fgets(message,sizeof(message),stdin);
+    printf("Enter Transaction : ");
+    scanf(" %[^\n]", transaction);
 
-    if(strlen(message) <= 1)
-    {
-        printf("Message cannot be empty.\n");
+    send(clientSocket, transaction, sizeof(transaction), 0);
 
-        close(clientSocket);
-        return 0;
-    }
+    recv(clientSocket, reply, sizeof(reply), 0);
 
-    write(clientSocket,message,strlen(message));
-
-    printf("Message Sent Successfully.\n");
-
-    printf("Closing Connection...\n");
-    printf("Disconnected from Server.\n");
+    printf("\nServer : %s\n", reply);
 
     close(clientSocket);
-    printf("\n Thank you...\n");
+
     return 0;
 }
--- Task4_Network_programming/server.c
+++ Task4_Network_programming/server.c
@@ -1,92 +1,73 @@
+/*
+    Task 4
+    ABC Bank Server
+    Multi-threaded Server
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
-#include <sys/types.h>
-#include <sys/wait.h>
+#include <pthread.h>
 
-#define PORT 8080
+void *handleClient(void *arg)
+{
+    int client = *(int *)arg;
+    free(arg);
+
+    char msg[100];
+
+    recv(client, msg, sizeof(msg), 0);
+
+    printf("\nCustomer Request : %s\n", msg);
+
+    strcpy(msg, "Transaction Successful");
+
+    send(client, msg, sizeof(msg), 0);
+
+    printf("Customer Disconnected.\n");
+
+    close(client);
+
+    return NULL;
+}
 
 int main()
 {
-    int serverSocket, clientSocket;
-    struct sockaddr_in serverAddr;
-
-    char message[100];
+    int serverSocket;
+    struct sockaddr_in server;
 
     serverSocket = socket(AF_INET, SOCK_STREAM, 0);
 
-    if(serverSocket < 0)
-    {
-        printf("Socket creation failed.\n");
-        return 1;
-    }
+    server.sin_family = AF_INET;
+    server.sin_port = htons(5000);
+    server.sin_addr.s_addr = INADDR_ANY;
 
-    serverAddr.sin_family = AF_INET;
-    serverAddr.sin_port = htons(PORT);
-    serverAddr.sin_addr.s_addr = INADDR_ANY;
+    bind(serverSocket, (struct sockaddr *)&server, sizeof(server));
 
-    if(bind(serverSocket,
-           (struct sockaddr*)&serverAddr,
-           sizeof(serverAddr)) < 0)
-    {
-        printf("Bind Failed.\n");
-        close(serverSocket);
-        return 1;
-    }
+    listen(serverSocket, 5);
 
-    if(listen(serverSocket,5) < 0)
-    {
-        printf("Listen Failed.\n");
-        close(serverSocket);
-        return 1;
-    }
-
-    printf("TCP Server Started.\n");
-    printf("Waiting for client...\n");
+   
+    printf("      ABC Bank Server Started\n");
+    
 
     while(1)
     {
-        clientSocket = accept(serverSocket, NULL, NULL);
+        int *client = malloc(sizeof(int));
 
-        if(clientSocket < 0)
-        {
-            printf("Connection Failed.\n");
-            continue;
-        }
+        *client = accept(serverSocket, NULL, NULL);
 
-        pid_t pid = fork();
+        printf("\nCustomer Connected.\n");
 
-        if(pid == 0)
-        {
-            printf("\nClient Connected.\n");
+        pthread_t tid;
 
-            int bytes = read(clientSocket,
-                             message,
-                             sizeof(message)-1);
+        pthread_create(&tid, NULL, handleClient, client);
 
-            if(bytes > 0)
-            {
-                message[bytes] = '\0';
-
-                printf("Client Message: %s\n", message);
-            }
-            else
-            {
-                printf("Cleint disconnected without sending data.\n");
-            }
-
-            printf("Connection Closed.\n");
-
-            close(clientSocket);
-
-            exit(0);
-        }
-    printf("Session Completed");
-
-        close(clientSocket);
+        pthread_detach(tid);
     }
+
+    close(serverSocket);
 
     return 0;
 }
